c1a89081dba15b4704ed55c27f712a8ecf0a83e8
snehachitre-apps <sneha.chitre.usa@gmail.com>
2023-05-29 20:27:49 -0500

    String exercises 2 and 3 commit

---
+++ Exercises/StringExercises02/index.js
@@ -0,0 +1,22 @@
+let num = 112;
+
+//Returns 'undefined'.
+//console.log(num.length);
+
+//Use type conversion to print the length (number of digits) of an integer.
+
+//Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+
+let strnum = String(num);
+
+if (strnum.includes('.'))
+  console.log(strnum.length - 1);
+else
+  console.log(strnum.length);
+
+
+
+
+
+
+//Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
---
+++ Exercises/StringExercises03/index.js
@@ -0,0 +1,20 @@
+let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+
+// First, print out the dna strand in it's current state.
+console.log(dna);
+
+
+//a) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
+dna=dna.trim();
+console.log(dna.trim());
+
+//b) Change all of the letters in the dna string to UPPERCASE, then print the result.
+
+//console.log(dna.toUpperCase);
+dna=dna.trim();
+dna=dna.toUpperCase();
+
+//c) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
+//Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
+
+console.log(dna);
